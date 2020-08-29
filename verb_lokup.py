30ca5ecbb244b5d4f70c0cc2db352ba0a370a10f
Mike Benton <47730996+bikepunkmaggie@users.noreply.github.com>
2020-08-29 15:35:39 -0400

    Update verb_lokup.py
    Update with Python version, still need to work on output.

--- verb_lokup.py
+++ verb_lokup.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from urllib2 import urlopen
+import urllib.request
+import urllib.parse
 from bs4 import BeautifulSoup
 import csv
+import io
 
 FILE_NAME = "verb_and_tense.txt"
-BASE_URL = "http://dongsa.net/?search="
+BASE_URL = "https://koreanverb.app/?search="
 
-with open(FILE_NAME, 'rb') as f:
+with io.open(FILE_NAME, newline='', encoding='utf8') as f:
     row_list = []
-    reader = csv.reader(f, delimiter = ",")
+    reader = csv.reader(f, delimiter=",")
     for row in reader:
         row_list.append(row)
     verb_list = row_list[0]
@@ -17,14 +19,14 @@ with open(FILE_NAME, 'rb') as f:
 
 for verb in verb_list:
     response_list = []
-    html = urlopen(BASE_URL + verb).read()
+    url = (BASE_URL + urllib.parse.quote(verb))
+    html = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(html, "html.parser")
     conjugation_dict = {}
     for conjugation in soup.findAll("tr", {"class": "conjugation-row"}):
         type, value = map(lambda x: x.get_text(), conjugation.findAll("td"))
         conjugation_dict[type] = value
-    print verb
+    print (verb)
     for tense in tense_list:
-        print tense, conjugation_dict[tense]
-    print
+        print(tense, conjugation_dict[tense])
 
