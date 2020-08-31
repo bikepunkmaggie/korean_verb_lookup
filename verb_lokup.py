2dad08df6836176553a287a46ea19bf649cce8ec
Mike Benton <47730996+bikepunkmaggie@users.noreply.github.com>
2020-08-31 09:26:51 -0400

    Updated writing and a bit of clean up.  Added requirements.txt

--- verb_lokup.py
+++ verb_lokup.py
@@ -16,6 +16,7 @@ with io.open(FILE_NAME, 'r', newline='', encoding='utf8') as f:
         row_list.append(row)
     verb_list = row_list[0]
     tense_list = row_list[1]
+    
 conjugation_dict = []
 
 for verb in verb_list:
@@ -26,12 +27,9 @@ for verb in verb_list:
 
     for conjugation in soup.findAll("tr", {"class": "conjugation-row"}):
         type, value = map(lambda x: x.get_text(), conjugation.findAll("td"))
-
-        conjugation_dict.append('{\"Inflected\"' + ':\"' + value + '\",' + '\"dict\":[\"' + verb + '\"]}')
-
+        conjugation_dict.append('{\"inflected\"' + ':\"' + value + '\",' + '\"dict\":[\"' + verb + '\"]}')
 
 with io.open("test.json", "w", encoding='utf8') as myfile:
     myfile.write('[')
     myfile.write(",".join(map(str, conjugation_dict)))
     myfile.write(']')
-
